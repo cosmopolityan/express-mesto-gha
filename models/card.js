2c8af5591a3593dafd7dff159034deff0f6f94da
Mark <cosmopolityan@yandex.ru>
2022-06-30 20:24:21 +0300

    hotfix (models/cards)

--- models/card.js
+++ models/card.js
@@ -36,7 +36,7 @@ const cardSchema = new mongoose.Schema(
       required: true,
     },
     likes: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: [mongoose.Schema.Types.ObjectId],
       ref: 'user',
       default: [],
     },
